c4d2662cf763f2aac0360185a01ff3a829b8d924
Liliana <lilainita@gmail.com>
2022-07-25 21:30:59 -0500

    AAA Pattern

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -8,19 +8,27 @@ import {
   PaymentStepPage,
 } from "../page/index";
 
-const menuContentPage = new MenuContentPage();
-const productListPage = new ProductListPage();
-const shoppingCartPage = new ShoppingCartPage();
-const loginProcess = new LoginPage();
-const addressStep = new AddressStepPage();
-const shippingStep = new ShippingStepPage();
-const paymentStep = new PaymentStepPage();
-
-
 describe("Buy a t-shirt", () => {
+  let menuContentPage: MenuContentPage;
+  let productListPage: ProductListPage;
+  let shoppingCartPage: ShoppingCartPage;
+  let loginProcess: LoginPage;
+  let addressStep: AddressStepPage;
+  let shippingStep: ShippingStepPage;
+  let paymentStep: PaymentStepPage;
   const email = "aperdomobo@gmail.com";
   const passwd = "WorkshopProtractor";
   const msg = "Your order on My Store is complete.";
+
+  before(()=> {
+    menuContentPage = new MenuContentPage();
+    productListPage = new ProductListPage();
+    shoppingCartPage = new ShoppingCartPage();
+    loginProcess = new LoginPage();
+    addressStep = new AddressStepPage();
+    shippingStep = new ShippingStepPage();
+    paymentStep = new PaymentStepPage();
+  });
 
   it("then the t-shirt should be bought", () => {
     menuContentPage.visitMenuContentPage();
