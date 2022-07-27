871fdbbb9af0fdf2d0420a582b772e1a430900b4
Liliana <lilainita@gmail.com>
2022-07-26 19:02:20 -0500

    Using Filter and Find

--- cypress/page/products-list.page.ts
+++ cypress/page/products-list.page.ts
@@ -1,18 +1,27 @@
+/* eslint-disable require-jsdoc */
 class ProductListPage {
   private addToCartBtn: string;
   private proceedToCheckOutBtn: string;
+  private productContainer: string;
 
   constructor() {
     this.addToCartBtn = ".button-container a[title='Add to cart']";
     this.proceedToCheckOutBtn = ".button-container a[title='Proceed to checkout']";
+    this.productContainer = "#center_column > ul > li > div.product-container";
   }
 
-  public addToCart(): void {
-    cy.get(this.addToCartBtn).click();
+  public addToCart(productName: string): void {
+    this.findProductByName(productName).find(this.addToCartBtn).click();
   }
 
   public proceedToCheckOutPopUpWindow(): void {
     cy.get(this.proceedToCheckOutBtn).click();
+  }
+
+  private findProductByName(productName: string) {
+    return cy.get(this.productContainer).filter((k, el) => {
+      return (el.innerText.includes(productName));
+    });
   }
 }
 
